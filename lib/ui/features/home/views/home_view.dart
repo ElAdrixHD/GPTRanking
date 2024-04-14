326ca5660880e2e7f6b245fdc7e6dcf06e460e02
Adrian Muñoz <eladrixhd@gmail.com>
2024-04-14 19:17:17 +0200

    Added trailing_comma

--- lib/ui/features/home/views/home_view.dart
+++ lib/ui/features/home/views/home_view.dart
@@ -127,7 +127,7 @@ class _HomeViewState extends ConsumerState<HomeView> {
                       ),
                     );
                   },
-                )
+                ),
               ],
             ),
           );
